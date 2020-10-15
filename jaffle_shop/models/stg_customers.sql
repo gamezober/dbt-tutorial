fe30d923503a9e67bcfa0349529818133bbcb20b
Zack Ober <zackober@lmzacko05421.home>
2020-10-15 09:17:14 -0700

    docs and sources

--- jaffle_shop/models/stg_customers.sql
+++ jaffle_shop/models/stg_customers.sql
@@ -1,6 +1,30 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+{{
+	
+	config(
+		materialized='view'
+		)
 
-from raw.jaffle_shop.customers
+}}
+
+with source as 
+
+(
+
+	select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+rename as
+
+(
+
+select
+    s.id as customer_id,
+    s.first_name,
+    s.last_name
+from
+	source s
+
+)
+
+select	* from rename
